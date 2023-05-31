27b831b0a6f4d45fb4bca77d7b3d37d117197abb
Joseph Odulesi <Joe@Josephs-MacBook-Pro.local>
2023-05-31 08:30:23 +0100

    updated for auto ripple effect

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-$('#ripple').ripples({
-	resolution: 512,
-	dropRadius: 20,
-	perturbance: 0.005,
-	interactive: true
-});
+// $('#ripple').ripples({
+// 	resolution: 512,
+// 	dropRadius: 20,
+// 	perturbance: 0.005,
+// 	interactive: true
+// });
 
-$('#ripple').ripples('play')
+// $('#ripple').ripples('play')
+
+$(document).ready(function() {
+	$('#ripple-container').ripples({
+	  resolution: 512,
+	  dropRadius: 20,
+	  perturbance: 0.03
+	});
+
+	setInterval(createAutomaticRipple, 5000);
+
+	function createAutomaticRipple() {
+	  const containerWidth = $('#ripple-container').width();
+	  const containerHeight = $('#ripple-container').height();
+	  const x = Math.random() * containerWidth;
+	  const y = Math.random() * containerHeight;
+
+	  $('#ripple-container').ripples('drop', x, y, 20, 0.07);
+	}
+  });
